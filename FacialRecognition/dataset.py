ad604f04bce4dda9623454c9ba9b4aef735adb34
eyesaacli <40645885+eyesaacli@users.noreply.github.com>
2018-07-11 14:31:24 -0700

    Update and rename face_dataset.py to FacialRecognition/dataset.py

--- FacialRecognition/dataset.py
+++ FacialRecognition/dataset.py
@@ -1,23 +1,22 @@
 import cv2
 import os
 cam = cv2.VideoCapture(0)
-#cam.set(3, 640) # set video width
-#cam.set(4, 480) # set video height
+
 face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-# For each person, enter one numeric face id
-face_id = input('\n enter user id  and press <return> ==> ')
-print("\n [INFO] Initializing face capture. Look at the camera and wait ...");
+face_id = input('\n enter user id  and press <return> ==> ') # prompting user for id
+print("\n [INFO] Initializing face capture. Look at the camera and wait ...")
+
 # Initialize individual sampling face count
 count = 0
 while (True):
 	ret, img = cam.read()
-	#img = cv2.flip(img, -1) # flip video image vertically
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	faces = face_detector.detectMultiScale(gray, 1.3, 5)
 	for(x,y,w,h) in faces:
 		cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
 		count += 1
-		# Save the captured image into the datasets folder
+		
+		# Save the captured image in the datasets folder
 		cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h, x:x+w])
 		cv2.imshow('image', img)
 	k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
@@ -25,7 +24,8 @@ while (True):
 		break
 	elif count>=30:
 		break
-# Do a bit of cleanup
-print("\n [INFO] Exiting Program and cleanup  stuff");
+
+# Notify user of program termination
+print("\n [INFO] Exiting Program");
 cam.release()
 cv2.destroyAllWindows()
